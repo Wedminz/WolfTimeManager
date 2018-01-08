c5592522613c731b5275716b375b15f1ed390efc
Alexw <alex.wolf.inbox@gmail.com>
2018-01-08 19:03:38 +0200

    Create git project

---
+++ app/src/main/java/inbox/wolf/alex/wolftimemanager/ProjectList.java
@@ -0,0 +1,13 @@
+package inbox.wolf.alex.wolftimemanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProjectList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project_list);
+    }
+}
