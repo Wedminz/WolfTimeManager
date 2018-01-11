16d07b1d6203567f22dd7b2ed31f5b904bd49166
Alexw <alex.wolf.inbox@gmail.com>
2018-01-11 23:57:16 +0200

    Create class time Changer and update edit text in activities

--- app/src/main/java/inbox/wolf/alex/wolftimemanager/view/CreateTimer.java
+++ app/src/main/java/inbox/wolf/alex/wolftimemanager/view/CreateTimer.java
@@ -3,17 +3,102 @@ package inbox.wolf.alex.wolftimemanager.view;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import inbox.wolf.alex.wolftimemanager.R;
+import inbox.wolf.alex.wolftimemanager.view.timemanager.TimeChanger;
 
 public class CreateTimer extends AppCompatActivity {
 
-    public static final int LAYOUT = R.layout.dialog_create_timer;
+    private static final int LAYOUT = R.layout.dialog_create_timer;
+    private static long time, hr, min, sec;
+
+    @BindView(R.id.hr_text_view)
+    TextView hrTextView;
+    @BindView(R.id.min_text_view)
+    TextView minTextView;
+    @BindView(R.id.sec_text_view)
+    TextView secTextView;
+    TimeChanger timeChanger;
+
+    @BindView(R.id.add_hr)
+    ImageView addHr;
+    @BindView(R.id.reduce_hr)
+    ImageView reduceHr;
+    @BindView(R.id.add_min)
+    ImageView addMin;
+    @BindView(R.id.reduce_min)
+    ImageView reduceMin;
+    @BindView(R.id.add_sec)
+    ImageView addSec;
+    @BindView(R.id.reduce_sec)
+    ImageView reduceSec;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(LAYOUT);
+        ButterKnife.bind(this);
+        timeChanger = new TimeChanger(time);
+    }
 
+    @OnClick(R.id.add_hr)
+    void onAddHr(){
+        if(timeChanger.addTime(99, addHr, reduceHr)){
+            timeChanger.setTime(++hr);
+            hrTextView.setText(timeChanger.getFormatTime());
+        }
+    }
+
+    @OnClick(R.id.reduce_hr)
+    void onReduceHr(){
+        if(timeChanger.reduceTime(reduceHr, addHr)){
+            timeChanger.setTime(--hr);
+            hrTextView.setText(timeChanger.getFormatTime());
+        }
+    }
+
+    @OnClick(R.id.add_min)
+    void onAddMin(){
+        if(timeChanger.addTime(60, addMin, reduceMin)){
+            timeChanger.setTime(++min);
+            minTextView.setText(timeChanger.getFormatTime());
+        }
+    }
+
+    @OnClick(R.id.reduce_min)
+    void onReduceMin(){
+        if(timeChanger.reduceTime(reduceMin, addMin)){
+            timeChanger.setTime(--min);
+            minTextView.setText(timeChanger.getFormatTime());
+        }
+    }
+
+    @OnClick(R.id.add_sec)
+    void onAddSec(){
+        if(timeChanger.addTime(60, addSec, reduceSec)){
+            timeChanger.setTime(++sec);
+            secTextView.setText(timeChanger.getFormatTime());
+        }
+    }
+
+    @OnClick(R.id.reduce_sec)
+    void onReduceSec(){
+        if(timeChanger.reduceTime(reduceSec, addSec)){
+            timeChanger.setTime(--sec);
+            secTextView.setText(timeChanger.getFormatTime());
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        hr = 0;
+        min = 0;
+        sec = 0;
     }
 }
---
+++ app/src/main/java/inbox/wolf/alex/wolftimemanager/view/timemanager/TimeChanger.java
@@ -0,0 +1,66 @@
+package inbox.wolf.alex.wolftimemanager.view.timemanager;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.widget.ImageView;
+
+public class TimeChanger extends Activity{
+
+    private long time;
+    private String formatTime;
+
+    public static final int TIME_MIN_VALUE = 0;
+
+    public TimeChanger(long time) {
+        this.time = time;
+    }
+
+    @SuppressLint("DefaultLocale")
+    public String getFormatTime() {
+        formatTime = String.format("%02d",  time);
+        return formatTime;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TimeChanger that = (TimeChanger) o;
+
+        if (time != that.time) return false;
+        return formatTime != null ? formatTime.equals(that.formatTime) : that.formatTime == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (time ^ (time >>> 32));
+        result = 31 * result + (formatTime != null ? formatTime.hashCode() : 0);
+        return result;
+    }
+
+    public boolean addTime(int maxTime, ImageView add, ImageView reduce){
+        if(time < maxTime) {
+            reduce.setEnabled(true);
+            return true;
+        } else{
+            add.setEnabled(false);
+            return false;
+        }
+    }
+
+    public boolean reduceTime(ImageView reduce, ImageView add){
+        if (time > TIME_MIN_VALUE) {
+            add.setEnabled(true);
+            return true;
+        } else {
+            reduce.setEnabled(false);
+            return false;
+        }
+    }
+
+}
